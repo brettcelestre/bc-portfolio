f6cb00e1ea878e8d32278d139bae0b0e54579eb2
Brett Celestre <bcelestre@gmail.com>
2018-09-29 13:05:36 -0700

    Updates ShortFilmsSections component and styles

--- src/Components/ShortFilmsSections/ShortFilmsSections.js
+++ src/Components/ShortFilmsSections/ShortFilmsSections.js
@@ -22,10 +22,7 @@ const buildFolderName = (title) => {
 
 const requireAllImages = (data) => {
   data.forEach((film) => {
-
     const name = buildFolderName(film.title);
-    console.log('name = ', name);
-
     filmCoverImages[name] = {
       Small: require(`../../assets/gallery/short-films/${name}/covers/Banner_Small.jpg`),
       Medium: require(`../../assets/gallery/short-films/${name}/covers/Banner_Medium.jpg`),
@@ -45,67 +42,65 @@ class ShortFilmsSections extends Component {
       currentSize: 'Large'
     }
 
-    console.log('FILMS = ', films);
-
     this.buildFilmsPreviews = this.buildFilmsPreviews.bind(this);
     this.windowSize = this.windowSize.bind(this);
+    this.saveScrollSpot = this.saveScrollSpot.bind(this);
   }
 
   componentWillMount() {
-    // Finds window size
+    // Finds window size and sets currentSize
     this.windowSize(window.innerWidth, window.innerHeight)
+    // TODO: Check localStorage - BC.Portfolio.FilmScrollSpot and scroll to that spot
   }
 
-  // Move into utils. Possibly not since there is a setState inside
+  saveScrollSpot() {
+    // TODO: Store film section scrolling spot
+    //    localStorage.setItem(`BC.Portfolio.FilmScrollSpot`, currentScrollSpot);
+  }
+
   windowSize(width, height, set) {
     // Adjusts for navigation
-    width += 155;    
-
-    // Makes sure you're not zoomed in
-    if (set || !this.state.zoom) {
-
-      // Large Width
-      if (width >= 1500 && (this.state.currentSize !== 'Large' || set)) {
-        if (set) return 'Large';
-        this.setState({currentSize: 'Large'});
-        console.log('windowSize = Large');
-        return;
-      }
-      // Medium Width
-      if (width >= 1100 && (this.state.currentSize !== 'Medium' || set)) {
-        if (set) return 'Medium';
-        this.setState({currentSize: 'Medium'});
-        console.log('windowSize = Medium');
-        return;
-      }
-      // Small Width
-      if (width <= 800 && (this.state.currentSize !== 'Small' || set)) {
-        if (set) return 'Small';
-        this.setState({currentSize: 'Small'});
-        console.log('windowSize = Small');
-        return;
-      }
+    width += 155;
+    // Large Width
+    if (width >= 1500 && (this.state.currentSize !== 'Large' || set)) {
+      if (set) return 'Large';
+      this.setState({currentSize: 'Large'});
+      return;
+    }
+    // Medium Width
+    if (width >= 1100 && (this.state.currentSize !== 'Medium' || set)) {
+      if (set) return 'Medium';
+      this.setState({currentSize: 'Medium'});
+      return;
+    }
+    // Small Width
+    if (width <= 800 && (this.state.currentSize !== 'Small' || set)) {
+      if (set) return 'Small';
+      this.setState({currentSize: 'Small'});
+      return;
     }
   }
 
   buildFilmsPreviews(){
     return this.state.films.data.map((film) => {
       return (
-        <div className="short-film-box">
+        <div className="short-film-box" id={film.title} key={film.title}>
 
-          <div className="short-film-cover-box">
-            <img
-              src={filmCoverImages[buildFolderName(film.title)][this.state.currentSize]}
-              // width={film.sizes[this.state.currentSize].width}
-              // height={film.sizes[this.state.currentSize].height}
-              alt="Del Rio"
-              className="short-film-cover-image"/>
-          </div>
+          <Link to={film.href} key={film.title} onClick={this.saveScrollSpot}>
+            <div className="short-film-cover-box">
+              <img
+                src={filmCoverImages[buildFolderName(film.title)][this.state.currentSize]}
+                // width={film.sizes[this.state.currentSize].width}
+                // height={film.sizes[this.state.currentSize].height}
+                alt={film.title}
+                className="short-film-cover-image"/>
+            </div>
 
-          <div className="short-film-details">
-            <h3>{film.title}</h3>
-            <h4>{film.year} | {film.length}</h4>
-          </div>
+            <div className="short-film-details">
+              <h3>{film.title}</h3>
+              <h4>{film.year} | {film.length}</h4>
+            </div>
+          </Link>
 
         </div>
       )
@@ -116,10 +111,7 @@ class ShortFilmsSections extends Component {
     return (
       <div className="short-films-sections">
         <WindowResizeListener
-          /*
-          TODO: Get debounce to work. Currently at 100
-          */
-         DEBOUNCE_TIME={4000}
+          DEBOUNCE_TIME={4000}
           onResize={windowSize => {
             this.windowSize(windowSize.windowWidth, windowSize.windowHeight)
           }
