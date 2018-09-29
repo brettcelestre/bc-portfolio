1126b70f181cd1ca728ee73b0155fa649735aa8f
Brett Celestre <bcelestre@gmail.com>
2018-09-29 14:29:45 -0700

    Cleans up About component & styles

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -3,8 +3,6 @@ import React, { Component } from 'react';
 import { Link, Redirect } from 'react-router-dom';
 import './About.css';
 
-// import { data, tags } from '../../assets/data/links.js';
-
 class About extends Component {
 
   constructor(props){
@@ -12,13 +10,46 @@ class About extends Component {
     this.state = {
     }
 
-    // this.buildLinks = this.buildLinks.bind(this);
+    this.buildGalleryCarousel = this.buildGalleryCarousel.bind(this);
+    this.buildAboutBio = this.buildAboutBio.bind(this);
+  }
+
+  buildGalleryCarousel() {
+    // TODO: Add images to carousel and controls
+    return (
+      <div className="about-gallery">
+        <div className="gallery-images">
+          Pictures
+        </div>
+        <div className="gallery-controls">
+          Image Index
+        </div>
+      </div>
+    )
+  }
+
+  buildAboutBio() {
+    // TODO: Add full bio
+    const text = () => {
+      return `
+      About Bio
+      `;
+    }
+
+    return (
+      <div className="about-bio">
+        <div className="bio-content">
+          {text()}
+        </div>
+      </div>
+    )
   }
 
   render() {
     return (
       <div className="about">
-        about
+        {this.buildGalleryCarousel()}
+        {this.buildAboutBio()}
       </div>
     );
   }
