94aa70e9f3c8c791821b0a07de45d725b11ea68a
Brett Celestre <bcelestre@gmail.com>
2018-09-29 18:30:14 -0700

    Adds client photography data back in

--- src/assets/data/photography_client.js
+++ src/assets/data/photography_client.js
@@ -1,226 +1,225 @@
 const ymlLoader = require('js-yaml');
 const data = ymlLoader.load(`
-  - title: TCI 01
-    date: 2010 DECEMBER
-    size: ''
-    description: D800, Photoshop
-    arrows: dark
-    info: ''
-    sizes:
-      Large:
-        width: 1500
-        height: 1000
-      Medium:
-        width: 1100
-        height: 733
-      Small:
-        width: 800
-        height: 533
+- title: TCI 01
+  date: 2010 DECEMBER
+  size: ''
+  description: D800, Photoshop
+  arrows: dark
+  info: ''
+  sizes:
+    Large:
+      width: 1500
+      height: 1000
+    Medium:
+      width: 1100
+      height: 733
+    Small:
+      width: 800
+      height: 533
 
-  - title: TCI 02
-    date: 2010 DECEMBER
-    size: ''
-    description: D800, Photoshop
-    arrows: dark
-    info: ''
-    sizes:
-      Large:
-        width: 1500
-        height: 988
-      Medium:
-        width: 1100
-        height: 725
-      Small:
-        width: 800
-        height: 527
+- title: TCI 02
+  date: 2010 DECEMBER
+  size: ''
+  description: D800, Photoshop
+  arrows: dark
+  info: ''
+  sizes:
+    Large:
+      width: 1500
+      height: 988
+    Medium:
+      width: 1100
+      height: 725
+    Small:
+      width: 800
+      height: 527
 
-  - title: TCI 03
-    date: 2010 DECEMBER
-    size: ''
-    description: D800, Photoshop
-    arrows: dark
-    info: ''
-    sizes:
-      Large:
-        width: 1090
-        height: 1200
-      Medium:
-        width: 817
-        height: 900
-      Small:
-        width: 545
-        height: 600
+- title: TCI 03
+  date: 2010 DECEMBER
+  size: ''
+  description: D800, Photoshop
+  arrows: dark
+  info: ''
+  sizes:
+    Large:
+      width: 1090
+      height: 1200
+    Medium:
+      width: 817
+      height: 900
+    Small:
+      width: 545
+      height: 600
 
-  - title: TCI 04
-    date: 2010 DECEMBER
-    size: ''
-    description: D800, Photoshop
-    arrows: dark
-    info: ''
-    sizes:
-      Large:
-        width: 1500
-        height: 952
-      Medium:
-        width: 1100
-        height: 698
-      Small:
-        width: 800
-        height: 508
+- title: TCI 04
+  date: 2010 DECEMBER
+  size: ''
+  description: D800, Photoshop
+  arrows: dark
+  info: ''
+  sizes:
+    Large:
+      width: 1500
+      height: 952
+    Medium:
+      width: 1100
+      height: 698
+    Small:
+      width: 800
+      height: 508
 
-  - title: TCI 08
-    date: 2010 DECEMBER
-    size: ''
-    description: D800, Photoshop
-    arrows: dark
-    info: ''
-    sizes:
-      Large:
-        width: 1500
-        height: 1001
-      Medium:
-        width: 1100
-        height: 734
-      Small:
-        width: 800
-        height: 534
+- title: TCI 05
+  date: 2010 DECEMBER
+  size: ''
+  description: D800, Photoshop
+  arrows: dark
+  info: ''
+  sizes:
+    Large:
+      width: 1500
+      height: 546
+    Medium:
+      width: 1100
+      height: 400
+    Small:
+      width: 800
+      height: 291
 
-  - title: TCI 09
-    date: 2010 DECEMBER
-    size: ''
-    description: D800, Photoshop
-    arrows: dark
-    info: ''
-    sizes:
-      Large:
-        width: 1500
-        height: 957
-      Medium:
-        width: 1100
-        height: 702
-      Small:
-        width: 800
-        height: 511
+- title: TCI 06
+  date: 2010 DECEMBER
+  size: ''
+  description: D800, Photoshop
+  arrows: dark
+  info: ''
+  sizes:
+    Large:
+      width: 1500
+      height: 609
+    Medium:
+      width: 1100
+      height: 447
+    Small:
+      width: 800
+      height: 325
 
-  - title: TCI 10
-    date: 2010 DECEMBER
-    size: ''
-    description: D800, Photoshop
-    arrows: dark
-    info: ''
-    sizes:
-      Large:
-        width: 1500
-        height: 1001
-      Medium:
-        width: 1100
-        height: 734
-      Small:
-        width: 800
-        height: 534
+- title: TCI 07
+  date: 2010 DECEMBER
+  size: ''
+  description: D800, Photoshop
+  arrows: dark
+  info: ''
+  sizes:
+    Large:
+      width: 1500
+      height: 609
+    Medium:
+      width: 1100
+      height: 447
+    Small:
+      width: 800
+      height: 325
 
-  - title: TCI 11
-    date: 2010 DECEMBER
-    size: ''
-    description: D800, Photoshop
-    arrows: dark
-    info: ''
-    sizes:
-      Large:
-        width: 1500
-        height: 807
-      Medium:
-        width: 1100
-        height: 592
-      Small:
-        width: 800
-        height: 431
+- title: TCI 08
+  date: 2010 DECEMBER
+  size: ''
+  description: D800, Photoshop
+  arrows: dark
+  info: ''
+  sizes:
+    Large:
+      width: 1500
+      height: 1001
+    Medium:
+      width: 1100
+      height: 734
+    Small:
+      width: 800
+      height: 534
 
-  - title: TCI 12
-    date: 2010 DECEMBER
-    size: ''
-    description: D800, Photoshop
-    arrows: dark
-    info: ''
-    sizes:
-      Large:
-        width: 1500
-        height: 1129
-      Medium:
-        width: 1100
-        height: 828
-      Small:
-        width: 800
-        height: 602
+- title: TCI 09
+  date: 2010 DECEMBER
+  size: ''
+  description: D800, Photoshop
+  arrows: dark
+  info: ''
+  sizes:
+    Large:
+      width: 1500
+      height: 957
+    Medium:
+      width: 1100
+      height: 702
+    Small:
+      width: 800
+      height: 511
 
-  - title: TCI 13
-    date: 2010 DECEMBER
-    size: ''
-    description: D800, Photoshop
-    arrows: dark
-    info: ''
-    sizes:
-      Large:
-        width: 1500
-        height: 772
-      Medium:
-        width: 1100
-        height: 566
-      Small:
-        width: 800
-        height: 412
+- title: TCI 10
+  date: 2010 DECEMBER
+  size: ''
+  description: D800, Photoshop
+  arrows: dark
+  info: ''
+  sizes:
+    Large:
+      width: 1500
+      height: 1001
+    Medium:
+      width: 1100
+      height: 734
+    Small:
+      width: 800
+      height: 534
+
+- title: TCI 11
+  date: 2010 DECEMBER
+  size: ''
+  description: D800, Photoshop
+  arrows: dark
+  info: ''
+  sizes:
+    Large:
+      width: 1500
+      height: 807
+    Medium:
+      width: 1100
+      height: 592
+    Small:
+      width: 800
+      height: 431
+
+- title: TCI 12
+  date: 2010 DECEMBER
+  size: ''
+  description: D800, Photoshop
+  arrows: dark
+  info: ''
+  sizes:
+    Large:
+      width: 1500
+      height: 1129
+    Medium:
+      width: 1100
+      height: 828
+    Small:
+      width: 800
+      height: 602
+
+- title: TCI 13
+  date: 2010 DECEMBER
+  size: ''
+  description: D800, Photoshop
+  arrows: dark
+  info: ''
+  sizes:
+    Large:
+      width: 1500
+      height: 772
+    Medium:
+      width: 1100
+      height: 566
+    Small:
+      width: 800
+      height: 412
 `);
 
 module.exports = { data };
-
-
-// - title: TCI 05
-// date: 2010 DECEMBER
-// size: ''
-// description: D800, Photoshop
-// arrows: dark
-// info: ''
-// sizes:
-//   Large:
-//     width: 1500
-//     height: 546
-//   Medium:
-//     width: 1100
-//     height: 400
-//   Small:
-//     width: 800
-//     height: 291
-
-// - title: TCI 06
-// date: 2010 DECEMBER
-// size: ''
-// description: D800, Photoshop
-// arrows: dark
-// info: ''
-// sizes:
-//   Large:
-//     width: 1500
-//     height: 609
-//   Medium:
-//     width: 1100
-//     height: 447
-//   Small:
-//     width: 800
-//     height: 325
-
-// - title: TCI 07
-// date: 2010 DECEMBER
-// size: ''
-// description: D800, Photoshop
-// arrows: dark
-// info: ''
-// sizes:
-//   Large:
-//     width: 1500
-//     height: 609
-//   Medium:
-//     width: 1100
-//     height: 447
-//   Small:
-//     width: 800
-//     height: 325
