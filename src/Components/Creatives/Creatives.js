6cd1917987aa80dc26a19be8e534ab6ef9c3ecb0
Brett Celestre <bcelestre@gmail.com>
2019-01-18 11:43:20 -0800

    Changes Friends component to Creatives

---
+++ src/Components/Creatives/Creatives.js
@@ -0,0 +1,160 @@
+
+import React, { Component } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import './Creatives.css';
+
+import { data, tags } from '../../assets/data/links';
+
+class Creatives extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      data,
+      tags,
+      filteredMediums: [],
+      visited: {}
+    }
+
+    this.buildLinks = this.buildLinks.bind(this);
+    this.buildTags = this.buildTags.bind(this);
+    this.filterByMedium = this.filterByMedium.bind(this);
+    this.buildFriends = this.buildFriends.bind(this);
+    this.hoverAnimation = this.hoverAnimation.bind(this);
+    this.buildSortDropdown = this.buildSortDropdown.bind(this);
+    this.removeMedium = this.removeMedium.bind(this);
+    this.visitingFriends = this.visitingFriends.bind(this);
+  }
+
+  buildLinks(links) {
+    // TODO: Finish building this function
+    return links.map((data) => {
+      const type = data.type.toUpperCase();
+      return(
+        <a href={data.url} className="type" target="blank">
+          {type}
+        </a>
+      )
+    });
+  }
+
+  buildTags(tags){
+    const mediums = tags.join(', ');
+    return(
+      <div className="tags">
+        {mediums}
+      </div>
+    )
+  }
+
+  hoverAnimation(data) {
+    // TODO: Create fast typewriter animation of persons name
+  }
+
+  filterByMedium(e) {
+    if (!this.state.filteredMediums.includes(e.target.value)) {
+      const newList = this.state.filteredMediums;
+      newList.push(e.target.value);
+      this.setState({
+        filteredMediums: newList
+      });
+    }
+  }
+
+  removeMedium(e) {
+    const target = e.target.id;
+    const newList = this.state.filteredMediums.reduce((acc, val) => {
+      if ( target !== val) {
+        acc.push(val);
+        return acc;
+      } else {
+        return acc;
+      }
+    }, []);
+    this.setState({
+      filteredMediums: newList
+    });
+  }
+
+  visitingFriends(friend) {
+    // console.log('friend', friend);
+  }
+
+  buildFriends() {
+    let friendsData = data;
+    
+    if (this.state.filteredMediums.length){
+      friendsData = data.reduce((acc, val) => {
+        let flag = false;
+        val.tags.forEach((medium) => {
+          if (this.state.filteredMediums.includes(medium)) {
+            flag = true;
+          }
+        });
+        if (flag) {
+          acc.push(val);
+          return acc;
+        }
+        return acc;
+      }, []);
+    }
+
+    return friendsData.map((data, i) => {
+      const primaryLink = data.links.find((val) => val.type === 'Portfolio') || data.links[0];
+      // TODO: Support multiple links for multiple platforms
+      return (
+        <a href={primaryLink.url} target="blank" key={data.title} onClick={this.visitingFriends} > {/* onMouseEnter={this.hoverAnimation} */}
+          <div className="friends-section" id={data.title}>
+            <h4><span className="highlight">{data.title}</span></h4>
+            {this.buildTags(data.tags)}
+            {/* {this.buildLinks(data.links)} */}
+          </div>
+        </a>
+      )
+    });
+  }
+
+  buildSortDropdown() {
+    const options = tags.map((data, i) => {
+      return (
+        <option value={data} key={data} id={data}>{data}</option>
+      )
+    });
+    // TODO: Create custom dropdown menu
+    return (
+      <select className="friends-sort-select" onChange={this.filterByMedium}>
+        <option disabled selected value> -- filter by medium -- </option>
+        {options}
+      </select>
+    )
+  }
+
+  buildFilteredMediums() {
+    return this.state.filteredMediums.map((data) => {
+      return (
+        <div className="filtered-option-box" onClick={this.removeMedium} key={`${data}-filter`} id={data}>
+          {data}
+          <div className="filtered-option-box-exit" onClick={this.removeMedium} id={data}>
+            X
+          </div>
+        </div>
+      )
+    });
+  }
+
+  render() {
+    return (
+      <div className="friends">
+        <div className="friends-sort-box">
+          {this.buildSortDropdown()}
+          {this.buildFilteredMediums()}
+        </div>
+        <div className="friends-columns">
+          {this.buildFriends()}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Creatives;
