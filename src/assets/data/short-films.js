e93f2b6171122d53cdb7eabe14b7bead2c451851
Brett Celestre <bcelestre@gmail.com>
2019-03-14 15:37:40 -0700

    Updates short film data with sections and credit formatting

--- src/assets/data/short-films.js
+++ src/assets/data/short-films.js
@@ -1,5 +1,6 @@
 
 const ymlLoader = require('js-yaml');
+
 const data = ymlLoader.load(`
 - title: Del Rio
   urlTitle: del-rio
@@ -7,44 +8,105 @@ const data = ymlLoader.load(`
   year: 2014
   length: "8:24"
   href: /short-films/del-rio
-  description:
+  description: Del Rio is a short thriller film, set in a grim deserted valley, baptized with the same title. As an extravagant locomotive, the Loxodonta Express, navigates through carrying a prosperous assortment of passengers and wealth; their destination will collide with the horrific dangers that unknowingly lay directly in their path.
   website: https://www.instagram.com/delriofilm/?hl=en
+  instagram: "@delriofilm"
+  imdb: https://www.imdb.com/title/tt2334592
   id: h8nbA3BLtbI
   url: https://www.youtube.com/embed/h8nbA3BLtbI?rel=0&showinfo=0&autoplay=1&version=3&enablejsapi=1
   embeded: <iframe width="560" height="315" src="https://www.youtube.com/embed/h8nbA3BLtbI" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-  info: Written & Directed by Matthew & Brett Celestre</ br></ br> \
+  options: 
+    - name: Behind The Scenes
+      href: /short-films/behind-the-scenes/del-rio
+      sizes:
+        Large:
+          width: 1500
+          height: 639
+        Medium:
+          width: 1100
+          height: 469
+        Small:
+          width: 800
+          height: 341
+    - name: Storyboards
+      href: /short-films/storyboards/del-rio
+      sizes:
+        Large:
+          width: 1500
+          height: 639
+        Medium:
+          width: 1100
+          height: 469
+        Small:
+          width: 800
+          height: 341
+    - name: Sketches
+      href: /artwork/sketches/del-rio
+      sizes:
+        Large:
+          width: 1500
+          height: 639
+        Medium:
+          width: 1100
+          height: 469
+        Small:
+          width: 800
+          height: 341
+  credits: <strong>Written & Directed</strong></ br>\
+        by</ br>\
+        Matthew & Brett Celestre</ br></ br></ br> \
         \
-        Original Music Written & Recorded by Ryan 'GOOMBAH' Schmidt</ br></ br> \
+        <strong>Original Music Written & Recorded</strong></ br>
+        by</ br>\
+        Ryan 'GOOMBAH' Schmidt</ br></ br></ br> \
         \
-        Music Mixed & Mastered by Ryan Schmidt & Andrew Jinkins</ br></ br> \
+        <strong>Music Mixed & Mastered</strong></ br>\
+        by</ br>\
+        Ryan Schmidt & Andrew Jinkins</ br></ br></ br> \
         \
-        Produced by Forbidden Science From Outer Space</ br></ br> \
+        <strong>Produced</strong></ br>\
+        by</ br>\
+        Forbidden Science From Outer Space</ br></ br></ br> \
         \
-        Executive Producers, Storyboards, Edited & Lighting by Matthew & Brett Celestre</ br></ br> \
+        <strong>Executive Producers, Storyboards, Edited & Lighting</strong></ br>\
+        by</ br>\
+        Matthew & Brett Celestre</ br></ br></ br> \
         \
-        Cinematography & Special Effects by Brett Celestre \
+        <strong>Cinematography & Special Effects</strong></ br>\
+        by</ br>\
+        Brett Celestre</ br></ br></ br> \
         \
-        Starring\
-        Corey Duffel\
-        Jesse Gale\
-        Joel Jutgair\
-        Caden Honda\
-        Steve Ruge\
-        Ryan Schmidt\
-        Andrew Jinkins\
-        Andy Lee\
-        Kevin Vanier\
-        Mark Smallhoover\
-        Justin Felix\
+        <strong>Starring</strong></ br>\
+        Corey Duffel</ br>\
+        Jesse Gale</ br>\
+        Joel Jutgair</ br>\
+        Caden Honda</ br>\
+        Steve Ruge</ br>\
+        Ryan Schmidt</ br>\
+        Andrew Jinkins</ br>\
+        Andy Lee</ br>\
+        Kevin Vanier</ br>\
+        Mark Smallhoover</ br>\
+        Justin Felix</ br></ br></ br>\
         \
-        Additional Help From\
-        Jeanne-Marie Garcia, Allyza Leongson, Kevin Luker, Rachel Banks, Brendin Lowenthal, Camille Rose, Brittany Watkins, Travis Van Oosbree, Kathy Celestre, Steve Celestre, Grant Selland, Genie Lee, John Shumaker, Conner Macleod, Darren Busby, Allen Hsiao, Derek Keller, Scott Celestre, Lauren Watkins, Barbie Vanier, Devon Blood, Monica Demaray-Blood, Keith Reader, Stephen Chow, Morgan Foster, The Schmidt Family, Helen Celestre, & Rachelle Kintzer\
+        <strong>Additional Help From</strong></ br>\
+        Jeanne-Marie Garcia, Allyza Leongson, Kevin Luker, Rachel Banks, Brendin Lowenthal, Camille Rose, Brittany Watkins, Travis Van Oosbree, Kathy Celestre, Steve Celestre, Grant Selland, Genie Lee, John Shumaker, Conner Macleod, Darren Busby, Allen Hsiao, Derek Keller, Scott Celestre, Lauren Watkins, Barbie Vanier, Devon Blood, Monica Demaray-Blood, Keith Reader, Stephen Chow, Morgan Foster, The Schmidt Family, Helen Celestre, & Rachelle Kintzer</ br></ br></ br>\
         \
-        Thanks to our Donators\
-        Gordon Kilgore, Final Stop Films, Cedric Howe, Mark Keller, Cassie Marketos, Kyle Luman, Elisabeth Holm, Francelle, skasnotdead, Brian Boyle, Camille Rose, Olivia, Eugene Ahn, Sherrill Wagnor, Thiago Bessa, Sierra Echo Charlie, Jack, Shay Kornfeld, Daniel Green, Ryan McClain, Joel Jutagir, VelcroFathoms, Cilista, Sarah Watters, Brain Orr, Andrew Zhou, William Johnston, Jennifer Frevele, Barbie Vanier, The Performance Group, Suanne Deising, Brittany, Albert J. Ferretti, Jonathan Verducci, Jane Bers, Sarah Doherty, Woody Hunt, John Loomis, Kathryn Stewart, Scott Celestre, Steve & Kathy Celestre, George Royster, Ray Pond, & Gwen Sutton Young\
+        <strong>Thanks to our Donators</strong></ br>\
+        Gordon Kilgore, Final Stop Films, Cedric Howe, Mark Keller, Cassie Marketos, Kyle Luman, Elisabeth Holm, Francelle, skasnotdead, Brian Boyle, Camille Rose, Olivia, Eugene Ahn, Sherrill Wagnor, Thiago Bessa, Sierra Echo Charlie, Jack, Shay Kornfeld, Daniel Green, Ryan McClain, Joel Jutagir, VelcroFathoms, Cilista, Sarah Watters, Brain Orr, Andrew Zhou, William Johnston, Jennifer Frevele, Barbie Vanier, The Performance Group, Suanne Deising, Brittany, Albert J. Ferretti, Jonathan Verducci, Jane Bers, Sarah Doherty, Woody Hunt, John Loomis, Kathryn Stewart, Scott Celestre, Steve & Kathy Celestre, George Royster, Ray Pond, & Gwen Sutton Young</ br></ br></ br>\
         \
-        © 2014 Matthew & Brett Celestre
-  sizes:
+        <strong>© 2014 Matthew & Brett Celestre</strong>
+  cover:
+    Large:
+      width: 1500
+      height: 639
+    Medium:
+      width: 1100
+      height: 469
+    Small:
+      width: 800
+      height: 341
+  details:
     Large:
       width: 1500
       height: 639
@@ -61,18 +123,83 @@ const data = ymlLoader.load(`
   year: 2011
   length: "3:15"
   href: /short-films/ballroom-dance-floor
-  description:
+  description: An elegant aristocrat escorts his true love on an intoxicated journey through Loxodonta Forest to attend the exquisite Ballroom masquerade party at Gatsby's mountaintop estate.
+  imdb: https://www.imdb.com/title/tt2378248/
   website: 
   id: qSpbsE7gabM
   url: https://www.youtube.com/embed/qSpbsE7gabM?rel=0&showinfo=0&autoplay=1&version=3&enablejsapi=1
   embeded: <iframe width="560" height="315" src="https://www.youtube.com/embed/qSpbsE7gabM" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-  info: Written & Directed by Matthew & Brett Celestre
-    Original Song by Andrew Jinkins
-    Story by Andrew Jinkins, Matthew & Brett Celestre
-    Animated by Brett & Matthew Celestre
-    Colored by Brett Celestre
+  options: 
+    - name: Behind The Scenes
+      href: /short-films/behind-the-scenes/ballroom-dance-floor
+    - name: Storyboards
+      href: /short-films/storyboards/ballroom-dance-floor
+    - name: Drawings
+      href: /artwork/drawings/ballroom-dance-floor
+  credits: <strong>Written & Directed</strong></ br>
+    by</ br>
+    Matthew & Brett Celestre</ br></ br></ br>
+    <strong>Original Song</strong></ br>
+    by</ br>
+    Andrew Jinkins</ br></ br></ br>
+    <strong>Story</strong></ br>
+    by</ br>
+    Andrew Jinkins, Matthew & Brett Celestre</ br></ br></ br>
+    <strong>Animated</strong></ br>
+    by</ br>
+    Brett & Matthew Celestre</ br></ br></ br>
+    <strong>Colored</strong></ br>
+    by</ br>
+    Brett Celestre</ br></ br></ br>
+    <strong>© Second Vision Productions 2011</strong>
+  sizes:
+    Large:
+      width: 1500
+      height: 639
+    Medium:
+      width: 1100
+      height: 469
+    Small:
+      width: 800
+      height: 341
 
-    © Second Vision Productions 2011
+- title: The Flying Disc
+  urlTitle: the-flying-disc
+  date: 2012 MAY
+  year: 2012
+  length: "1:18"
+  href: /short-films/the-flying-disc
+  description: One disc. One newspaper. One phone call. Major disappointment.
+  website: 
+  id: aw3WzAiKgY8
+  url: https://www.youtube.com/embed/aw3WzAiKgY8?rel=0&showinfo=0&autoplay=1&version=3&enablejsapi=1
+  embeded: <iframe width="560" height="315" src="https://www.youtube.com/embed/aw3WzAiKgY8" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+  options: 
+    - name: Sketches
+      href: /artwork/sketches/the-flying-disc
+  credits: Written, Directed, & Special Effects by Brett Celestre
+  sizes:
+    Large:
+      width: 1500
+      height: 639
+    Medium:
+      width: 1100
+      height: 469
+    Small:
+      width: 800
+      height: 341
+
+- title: Dan Pool
+  urlTitle: dan-pool
+  date: 2011 September
+  year: 2011
+  length: "1:43"
+  href: /short-films/dan-pool
+  description: Deep underground in San Jose State University's Recreational Center one man is on a mission to have the perfect game of billards. His single shot could change the world, but on this day fate had different plans.
+  id: LqBw8UVvfXg
+  url: https://www.youtube.com/embed/LqBw8UVvfXg?rel=0&showinfo=0&autoplay=1&version=3&enablejsapi=1
+  options: 
+  credits: Written & Directed by Brett Celestre 
   sizes:
     Large:
       width: 1500
@@ -95,7 +222,11 @@ const data = ymlLoader.load(`
   id: KCizRihPlU0
   url: https://www.youtube.com/embed/KCizRihPlU0?rel=0&showinfo=0&autoplay=1&version=3&enablejsapi=1
   embeded: <iframe width="560" height="315" src="https://www.youtube.com/embed/KCizRihPlU0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-  info: Written & Directed by Matthew & Brett Celestre
+  options:
+    - name: Drawings
+      href: /artwork/Drawings/justin-felix-vs-gavin-drago
+  viewFullCredits: true
+  credits: Written & Directed by Matthew & Brett Celestre
     Animated by Brett & Matthew Celestre
 
     Voice Actors
@@ -124,48 +255,6 @@ const data = ymlLoader.load(`
 module.exports = { data };
 
 
-// - title: The Flying Disc
-//   urlTitle: the-flying-disc
-//   date: 2012 MAY
-//   year: 2012
-//   length: "1:18"
-//   href: /short-films/the-flying-disc
-//   description:
-//   website: 
-//   id: aw3WzAiKgY8
-//   url: https://www.youtube.com/embed/aw3WzAiKgY8?rel=0&showinfo=0&autoplay=1&version=3&enablejsapi=1
-//   embeded: <iframe width="560" height="315" src="https://www.youtube.com/embed/aw3WzAiKgY8" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-//   info: Written, Directed, & Special Effects by Brett Celestre
-//   sizes:
-//     Large:
-//       width: 1500
-//       height: 639
-//     Medium:
-//       width: 1100
-//       height: 469
-//     Small:
-//       width: 800
-//       height: 341
-
-// - title: Dan Pool
-//   urlTitle: dan-pool
-//   date: 2011 September
-//   year: 2011
-//   length: "1:43"
-//   href: /short-films/dan-pool
-//   id: LqBw8UVvfXg
-//   url: https://www.youtube.com/embed/LqBw8UVvfXg?rel=0&showinfo=0&autoplay=1&version=3&enablejsapi=1
-//   fontStyles:
-//   sizes:
-//     Large:
-//       width: 1500
-//       height: 639
-//     Medium:
-//       width: 1100
-//       height: 469
-//     Small:
-//       width: 800
-//       height: 341
 
 // real life 2
 // real life 
